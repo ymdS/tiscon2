abc1a17bc8edf9fcdd063e3e041bd76f6ce88f38
blackawa <3104kurokawa@gmail.com>
2017-02-23 00:24:42 +0900

    remove signature attribute table

--- src/main/java/db/migration/V4__InsertUser.java
+++ src/main/java/db/migration/V4__InsertUser.java
@@ -8,7 +8,7 @@ import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 /**
  * @author kawasima
  */
-public class V5__InsertUser implements JdbcMigration {
+public class V4__InsertUser implements JdbcMigration {
     @Override
     public void migrate(Connection connection) throws Exception {
         String sql = "INSERT INTO user(user_id, first_name, last_name, email, pass) " +
